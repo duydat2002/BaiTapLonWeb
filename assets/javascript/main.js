ad19383109fda747ddecf5ec52d06567f022fc2d
Duy Dat <duydat30122002@gmail.com>
2022-02-27 21:48:49 +0700

    Update Slider Pagination

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -26,9 +26,7 @@ window.onclick = (e) => {
 function slider(sliderText, transitionText, timeAuto) {
     const sliderCotainer = document.querySelector(`.${sliderText} .slider-container`);
     const item = sliderCotainer.querySelector(".slider-wrapper");
-    const prev = sliderCotainer.querySelector(".slider-prev");
-    const next = sliderCotainer.querySelector(".slider-next");
-
+    const sliderItems = sliderCotainer.querySelectorAll(".slider-item");
     var isPause = false;
     var isSliderRun = false;
     var slideIndex = 1;
@@ -36,35 +34,73 @@ function slider(sliderText, transitionText, timeAuto) {
     var slideWidth = item.children[0].clientWidth;;
     const slideCount = item.childElementCount;
     
+    //Numbered
+    for (let i=0; i<slideCount; i++) {
+        sliderItems[i].dataset.index = i;
+    }
+
     //Resize
     window.addEventListener("resize", () => {
-        slideWidth = item.children[0].clientWidth;
+        slideWidth = sliderItems[0].clientWidth;
         item.style.transform = `translateX(${-slideWidth}px)`;
     })
 
     //Clone child
-    const firstElement = item.firstElementChild;
-    const lastElement = item.lastElementChild;
-    item.appendChild(firstElement.cloneNode(true));
-    item.prepend(lastElement.cloneNode(true));
+    item.appendChild(sliderItems[0].cloneNode(true));
+    item.prepend(sliderItems[slideCount-1].cloneNode(true));
     
+    item.firstElementChild.classList.add("slider-clone");
+    item.lastElementChild.classList.add("slider-clone");
+
     item.style.transform = `translateX(${-slideWidth}px)`;
     item.children[slideIndex].classList.add("active");
 
-    //Control
-    prev.onclick = () => {
-        if (!isSliderRun) {
-            slideIndex--;
-            item.style.transition = transitionText;
-            item.style.transform = `translateX(${-slideIndex*slideWidth}px)`;
+    //Navigation
+    if (sliderCotainer.querySelector(".slider-navigation")) {
+        const prev = sliderCotainer.querySelector(".slider-prev");
+        const next = sliderCotainer.querySelector(".slider-next");
+
+        prev.onclick = () => {
+            if (!isSliderRun) {
+                slideIndex--;
+                item.style.transition = transitionText;
+                item.style.transform = `translateX(${-slideIndex*slideWidth}px)`;
+            }
+        }
+        next.onclick = () => {
+            if (!isSliderRun) {
+                slideIndex++;
+                item.style.transition = transitionText;
+                item.style.transform = `translateX(${-slideIndex*slideWidth}px)`;
+            }
         }
     }
-    next.onclick = () => {
-        if (!isSliderRun) {
-            slideIndex++;
-            item.style.transition = transitionText;
-            item.style.transform = `translateX(${-slideIndex*slideWidth}px)`;
+    
+    //Pagination
+    if (sliderCotainer.querySelector(".slider-pagination")) {
+        const pagination = sliderCotainer.querySelector(".slider-pagination");
+
+        for (let i=0; i<slideCount; i++) {
+            pagination.innerHTML += `<span class="slider-dot" data-dotindex="${i}"></span>`;
         }
+
+        const dots = pagination.querySelectorAll(".slider-dot");
+        dots.forEach((dot) => {
+            dot.onclick = () => {
+                slideIndex = 1 + parseInt(dot.dataset.dotindex);
+                item.style.transition = transitionText;
+                item.style.transform = `translateX(${-slideIndex*slideWidth}px)`;
+                console.log(slideIndex);
+            }
+        })
+
+        pagination.children[0].classList.add("active");
+
+        item.addEventListener("transitionstart", () => {
+            const dotActiveIndex = item.children[slideIndex].dataset["index"];
+            pagination.querySelector(".slider-dot.active").classList.remove("active");
+            pagination.children[dotActiveIndex].classList.add("active");
+        })
     }
 
     //Auto
@@ -90,7 +126,14 @@ function slider(sliderText, transitionText, timeAuto) {
     //Loop
     item.addEventListener("transitionstart", () => {
         isSliderRun = true;
-        item.querySelector(".active").classList.remove("active");
+        item.querySelector(".active")?.classList.remove("active");
+        var activeIndex = slideIndex;
+        if (activeIndex == slideCount + 1)
+            activeIndex = 1;
+        if (activeIndex == 0)
+            activeIndex = slideCount;
+        item.children[activeIndex].classList.add("active");
+
     })
     
     item.addEventListener("transitionend", () => {
@@ -105,14 +148,11 @@ function slider(sliderText, transitionText, timeAuto) {
             item.style.transition = "none";
             item.style.transform = `translateX(${-slideIndex*slideWidth}px)`;
         }
-        item.children[slideIndex].classList.add("active");
     })
 }
 
 slider("home-banner__slider", "0.5s", 5000);
 
-
-var items = document.querySelector(".slider-wrapper");
 
 
 
